a5066e137c847a64bfd04b146921402a85887497
SoftwareEngineer001 <top.david0817@gmail.com>
2023-08-31 17:04:45 +0900

    add second commit

--- base/views.py
+++ base/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Room
+from . forms import RoomForm
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 def room(request, pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id = pk)
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
