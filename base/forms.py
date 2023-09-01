6918c9656e07460c8ae2d770eb0d57274d4a5f6e
SoftwareEngineer001 <top.david0817@gmail.com>
2023-09-02 04:12:43 +0900

    complete project

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from . models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- base/urls.py
+++ base/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('update-user/', views.updateUser, name="update-user"),
 
 ]
